d4c57dbb5770bdb96bee8cf4af1d760dd09d0808
dimaSkalora <timon2@ukr.net>
2018-01-03 12:39:57 +0200

    8 03 HW07 formatters

---
+++ src/main/java/ru/javawebinar/topjava/web/converter/DateTimeFormatters.java
@@ -0,0 +1,44 @@
+package ru.javawebinar.topjava.web.converter;
+
+import org.springframework.format.Formatter;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
+
+/**
+ * gkislin
+ * 25.10.2016
+ */
+public class DateTimeFormatters {
+    public static class LocalDateFormatter implements Formatter<LocalDate> {
+
+        @Override
+        public LocalDate parse(String text, Locale locale) throws ParseException {
+            return parseLocalDate(text);
+        }
+
+        @Override
+        public String print(LocalDate lt, Locale locale) {
+            return lt.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        }
+    }
+
+    public static class LocalTimeFormatter implements Formatter<LocalTime> {
+
+        @Override
+        public LocalTime parse(String text, Locale locale) throws ParseException {
+            return parseLocalTime(text);
+        }
+
+        @Override
+        public String print(LocalTime lt, Locale locale) {
+            return lt.format(DateTimeFormatter.ISO_LOCAL_TIME);
+        }
+    }
+}
